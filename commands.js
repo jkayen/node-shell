c388b939ee06b7be2722478d9b10934c36e9f48c
Jacob Kayen <jacob.kayen@gmail.com>
2017-03-06 13:23:23 -0500

    head cmd written

--- commands.js
+++ commands.js
@@ -2,21 +2,12 @@ var cmd = module.exports = {}
 var fs = require('fs')
 
 cmd.pwd = function(file){
-  //var command = data.toString().trim(); // remove the newline
-  //if(command === 'pwd'){
-    //console.log(process.env)
     console.log(process.env.PWD)
-  //}
-  process.stdout.write('\nprompt > ');
 }
 
 cmd.date = function(file){
-  //var command = data.toString().trim();
-  //if (command === 'date') {
     var date = new Date()
     console.log(date.toUTCString())
-  //}
-  process.stdout.write('\nprompt > ');
 }
 
 cmd.ls = function(file) {
@@ -25,38 +16,36 @@ cmd.ls = function(file) {
     files.forEach(function(file) {
       process.stdout.write(file.toString() + "\n");
     })
-    process.stdout.write("prompt > ");
   });
 }
 
 cmd.echo = function(file) {
-  //need to fix PATH
-  if (file === '$') {
-    var action = file.slice(1).trim();
+  if (file[0] === '$') {
+    var action = file.slice(1);
     var output = process.env[action];
     console.log(output);
   } else {
     console.log(file);
   } 
-  process.stdout.write("prompt > ");
 }
 
 cmd.cat = function(file) {
-  var f = fs.readFile(o, 'utf8', (err, data) => {
+  fs.readFile(o, 'utf8', (err, data) => {
     if (err) throw err;
     console.log(data);
   });
-  console.log(f);
-  process.stdout.write("prompt > ");
 }
 
 cmd.head = function(file) {
-  var f = fs.readFile(file, 'utf8', (err, data) => {
+  fs.readFile(file, 'utf8', (err, data) => {
     if (err) throw err;
-    console.log(data);
+    var one = data.indexOf('\n')
+    var two = data.indexOf('\n', one + 1)
+    var three = data.indexOf('\n', two + 1)
+    var four = data.indexOf('\n', three + 1)
+    var five = data.indexOf('\n', four + 1)
+    console.log(data.slice(0, five));
   });
-  console.log(f);
-  process.stdout.write("prompt > ");  
 }
 
 cmd.tail = function(file) {
