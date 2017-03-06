e90412f78a5a3cb0cff8da100cee64eaf0b25d45
Jacob Kayen <jacob.kayen@gmail.com>
2017-03-06 12:59:53 -0500

    working on head cmd

--- bash.js
+++ bash.js
@@ -2,29 +2,29 @@
 // console.log('process keys')
 // console.log(Object.keys(process))
 
+var cmd = require('./commands.js')
 //Output a prompt
 
-var cmd = require('./commands.js')
 process.stdout.write('prompt > ');
 
 // The stdin 'data' event fires after a user types in a line
 process.stdin.on('data', function (data) {
-  var command = data.toString().trim(); // remove the newline
-  if(command === 'pwd'){
-    //console.log(process.env.PWD)
+  var command = data.toString().slice(0, data.toString().indexOf(' ')); // remove the newline
+  var input = data.toString().slice(data.toString().indexOf(' ') + 1).trim();
+  if (command === 'pwd'){
     cmd.pwd()
-  }
-  //cmd.pwd()
-  else if (command === 'date') {
-    //var date = new Date()
-    //console.log(date.toUTCString())
+  } else if (command === 'date') {
     cmd.date()
+  } else if (command === 'ls') {
+    cmd.ls()
+  } else if (command === 'echo') {
+    cmd.echo(input)
+  } else if (command === 'cat') {
+    cmd.cat(input);
+  } else if (command === 'head') {
+    cmd.head(input);
   }
-  //cmd.date()
-  // process.stdout.write('You typed: ' + cmd);
-  // process.stdout.write('\nprompt > ');
 
 });
-// process.stdin.on('data', function(data){
-//
-// })
+
+
--- commands.js
+++ commands.js
@@ -1,6 +1,7 @@
 var cmd = module.exports = {}
+var fs = require('fs')
 
-cmd.pwd = function(){
+cmd.pwd = function(file){
   //var command = data.toString().trim(); // remove the newline
   //if(command === 'pwd'){
     //console.log(process.env)
@@ -9,11 +10,55 @@ cmd.pwd = function(){
   process.stdout.write('\nprompt > ');
 }
 
-cmd.date = function(){
+cmd.date = function(file){
   //var command = data.toString().trim();
   //if (command === 'date') {
     var date = new Date()
     console.log(date.toUTCString())
   //}
   process.stdout.write('\nprompt > ');
+}
+
+cmd.ls = function(file) {
+  fs.readdir('.', function(err, files) {
+    if (err) throw err;
+    files.forEach(function(file) {
+      process.stdout.write(file.toString() + "\n");
+    })
+    process.stdout.write("prompt > ");
+  });
+}
+
+cmd.echo = function(file) {
+  //need to fix PATH
+  if (file === '$') {
+    var action = file.slice(1).trim();
+    var output = process.env[action];
+    console.log(output);
+  } else {
+    console.log(file);
+  } 
+  process.stdout.write("prompt > ");
+}
+
+cmd.cat = function(file) {
+  var f = fs.readFile(o, 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
+  console.log(f);
+  process.stdout.write("prompt > ");
+}
+
+cmd.head = function(file) {
+  var f = fs.readFile(file, 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+  });
+  console.log(f);
+  process.stdout.write("prompt > ");  
+}
+
+cmd.tail = function(file) {
+
 }
